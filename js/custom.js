4b0f7f9354bb68d7466e74b4ad379d421e0a361c
lennonprado <lennonprado@gmail.com>
2017-08-23 14:35:02 -0300

    Listo con 5 filtros

--- js/custom.js
+++ js/custom.js
@@ -1,63 +1,47 @@
-// Without JQuery
-var slider = new Slider('#ex1', {
-	formatter: function(value) {
-		return 'Current value: ' + value;
-	}
-});
 
 
-function chequeoImg(){
-  var i=document.getElementById('fileUp-input');
-  if(window.FileReader){
-  }
-}
+	var x = null;
+	var ctx = null;
+	var image1 = null;
+	var imageData = null;
+
+	var slider = new Slider('#ex1', {
+		formatter: function(value) {
+			return 'Current value: ' + value;
+		}
+	});
 
 
-function subir(){
-  	var x=document.getElementById('fileUp-input');
-		var ctx = document.getElementById("magicHere").getContext("2d");
-		var image1 = new Image();
+	function subir(){
+		x=document.getElementById('fileUp-input');
+		ctx = document.getElementById("magicHere").getContext("2d");
+		image1 = new Image();
 		image1.src=x.files[0].name;
 		image1.onload = function(){
 			ctx.drawImage(image1,0,0);
-			var imageData = ctx.getImageData(0,0,image1.width,image1.height);
+			imageData = ctx.getImageData(0,0,image1.width,image1.height);
 			ctx.putImageData(imageData,0,0);
 		}
-		document.getElementById('magicHere').style.display = 'block';
-		document.getElementById('fileUp-input').style.display = 'none';
-}
+		$('#magicHere').show();
+		$('.file-input-content').hide();
+	}
 
 
-function brighness(){
-		var x=document.getElementById('fileUp-input');
-		var ctx = document.getElementById("magicHere").getContext("2d");
-		var image1 = new Image();
-		image1.src=x.files[0].name;
-		image1.onload = function(){
-			ctx.drawImage(image1,0,0);
-			var imageData = ctx.getImageData(0,0,image1.width,image1.height);
-			for(var x=0; x<image1.width; x++){
-				for(var y=0; y<image1.height; y++){
-					var R = getRed(imageData,x,y);
-					var G = getGreen(imageData,x,y);
-					var B = getBlue(imageData,x,y);
-					setRed(imageData,x,y,R+80);
-					setGreen(imageData,x,y,G+80);
-					setBlue(imageData,x,y,B+80);
-				}
+	function brighness(){
+		for(var x=0; x<image1.width; x++){
+			for(var y=0; y<image1.height; y++){
+				var R = getRed(imageData,x,y);
+				var G = getGreen(imageData,x,y);
+				var B = getBlue(imageData,x,y);
+				setRed(imageData,x,y,R+10);
+				setGreen(imageData,x,y,G+10);
+				setBlue(imageData,x,y,B+10);
 			}
-			ctx.putImageData(imageData,0,0);
+		ctx.putImageData(imageData,0,0);
 		}
 	}
 
-function negative(){
-	var x=document.getElementById('fileUp-input');
-	var ctx = document.getElementById("magicHere").getContext("2d");
-	var image1 = new Image();
-	image1.src=x.files[0].name;
-	image1.onload = function(){
-		ctx.drawImage(image1,0,0);
-		var imageData = ctx.getImageData(0,0,image1.width,image1.height);
+	function negative(){
 		for(var x=0; x<image1.width; x++){
 			for(var y=0; y<image1.height; y++){
 				var R = getRed(imageData,x,y);
@@ -70,56 +54,95 @@ function negative(){
 		}
 		ctx.putImageData(imageData,0,0);
 	}
-}
 
-function grey(){
-		var x=document.getElementById('fileUp-input');
-		var ctx = document.getElementById("magicHere").getContext("2d");
-		var image1 = new Image();
-		image1.src=x.files[0].name;
-		image1.onload = function(){
-			ctx.drawImage(image1,0,0);
-			var imageData = ctx.getImageData(0,0,image1.width,image1.height);
-			for(var x=0; x<image1.width; x++){
-				for(var y=0; y<image1.height; y++){
-					var R = getRed(imageData,x,y);
-					var G = getGreen(imageData,x,y);
-					var B = getBlue(imageData,x,y);
-					var todos = (R+G+B)/3;
-					setRed(imageData,x,y,todos);
-					setGreen(imageData,x,y,todos);
-					setBlue(imageData,x,y,todos);
-				}
+	function grey(){
+		for(var x=0; x<image1.width; x++){
+			for(var y=0; y<image1.height; y++){
+				var R = getRed(imageData,x,y);
+				var G = getGreen(imageData,x,y);
+				var B = getBlue(imageData,x,y);
+				var todos = (R+G+B)/3;
+				setRed(imageData,x,y,todos);
+				setGreen(imageData,x,y,todos);
+				setBlue(imageData,x,y,todos);
 			}
-			ctx.putImageData(imageData,0,0);
 		}
+		ctx.putImageData(imageData,0,0);
 	}
 
 
 	function sepia(){
-		var x=document.getElementById('fileUp-input');
-		var ctx = document.getElementById("magicHere").getContext("2d");
-		var image1 = new Image();
-		image1.src=x.files[0].name;
-		image1.onload = function(){
-			ctx.drawImage(image1,0,0);
-			var imageData = ctx.getImageData(0,0,image1.width,image1.height);
-			for(var x=0; x<image1.width; x++){
-				for(var y=0; y<image1.height; y++){
-					var R = getRed(imageData,x,y);
-					var G = getGreen(imageData,x,y);
-					var B = getBlue(imageData,x,y);
-					var tr = (0.393 * R) + (0.769 * G) + (0.189 * B);
-					var tg = (0.349 * R) + (0.686 * G) + (0.168 * B);
-					var tb = (0.272 * R) + (0.534 * G) + (0.131 * B);
-					setRed(imageData,x,y,tr);
-					setGreen(imageData,x,y,tg);
-					setBlue(imageData,x,y,tb);
-				}
+		for(var x=0; x<image1.width; x++){
+			for(var y=0; y<image1.height; y++){
+				var R = getRed(imageData,x,y);
+				var G = getGreen(imageData,x,y);
+				var B = getBlue(imageData,x,y);
+				var tr = (0.393 * R) + (0.769 * G) + (0.189 * B);
+				var tg = (0.349 * R) + (0.686 * G) + (0.168 * B);
+				var tb = (0.272 * R) + (0.534 * G) + (0.131 * B);
+				setRed(imageData,x,y,tr);
+				setGreen(imageData,x,y,tg);
+				setBlue(imageData,x,y,tb);
 			}
-			ctx.putImageData(imageData,0,0);
 		}
+		ctx.putImageData(imageData,0,0);
 	}
+
+
+
+	function binarization() {
+        quantity = $('#ex1').val();
+        var data = imageData;
+        console.log(quantity);
+        for(var x=0; x<image1.width; x++){
+            for(var y=0; y<image1.height; y++){
+            	//red channel
+                if(getRed(data,x,y)>quantity)
+                    setRed(data,x,y,255);
+				else
+	                setRed(data,x,y,0);
+				// green channel
+				if(getGreen(data,x,y)>quantity)
+                    setGreen(data,x,y,255);
+				else
+                    setGreen(data,x,y,0);
+				// blue channel
+				if(getBlue(data,x,y)>quantity)
+                    setBlue(data,x,y,255);
+				else
+	                setBlue(data,x,y,0);
+            }
+        }
+        ctx.putImageData(data,0,0);
+    }
+
+    function smoothing(){
+        for(var x=0; x<image1.width; x++){
+            for(var y=0; y<image1.height; y++){
+				// around
+				cantR = 0;
+                cantG = 0;
+                cantB = 0;
+				sum = 0;
+				for(j=x-1;j<=x+1;j++) {
+                    for (k = y - 1; k <= y + 1; k++) {
+                    	if((j>0) && (k>0)) {
+                            cantR = cantR + getRed(imageData, j, k);
+                            cantG = cantG + getGreen(imageData, j, k);
+                            cantB = cantB + getBlue(imageData, j, k);
+                            sum++;
+                        }
+                    }
+                }
+                setRed(imageData,x,y,cantR/sum);
+                setGreen(imageData,x,y,cantG/sum);
+                setBlue(imageData,x,y,cantB/sum);
+            }
+        }
+        ctx.putImageData(imageData,0,0);
+    }
+
+
 
 	function getRed(imageData,x,y){
 		var index = (x + y * imageData.width) * 4;
